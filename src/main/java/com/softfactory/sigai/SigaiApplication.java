08b76f0de3627e2fc826d5305f6d079adb9be69e
omar.kourmou <kourmou.omar@gmail.com>
2020-10-21 20:30:18 +0100

    [kourmouOmar][structure backen sigai]

---
+++ src/main/java/com/softfactory/sigai/SigaiApplication.java
@@ -0,0 +1,16 @@
+package com.softfactory.sigai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SigaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/softfactory/sigai/config/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.softfactory.sigai.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+}
