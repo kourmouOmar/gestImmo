ec0dc4db6347b7363777e43026562cdf602dc7d5
omarkourmou <kourmou.omar@gmail.com>
2021-04-14 04:15:03 +0000

    <omarkourmou><update pom.xml for user mariadb and application.properties >

--- src/main/java/com/softfactory/sigai/repository/UserRepository.java
+++ src/main/java/com/softfactory/sigai/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.softfactory.sigai.entities.UserEntity;
@@ -11,11 +12,28 @@ import com.softfactory.sigai.entities.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	
+	/**
+	 * find user by username
+	 * 
+	 * @param username
+	 * @return
+	 */
 	UserEntity findByUsername(String username);
 	
+	/**
+	 * get all users
+	 * 
+	 * @return
+	 */
 	@Query("SELECT u FROM UserEntity u")
 	List<UserEntity> getAllUsers();
 	
-	@Query("SELECT u from UserEntity WHERE u.id = !1")
-	UserEntity findUserById(Long idUser);
+	/**
+	 * find user by id
+	 * 
+	 * @param idUser
+	 * @return
+	 */
+	@Query("SELECT u from UserEntity u WHERE u.id =: id")
+	UserEntity findUserById(@Param("id") Long idUser);
 }
